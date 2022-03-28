42ecef983508ece6b517efebc15d7d1be780ae13
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-28 15:35:02 -0400

    Added some code

--- TodoListApp/ViewController.swift
+++ TodoListApp/ViewController.swift
@@ -7,12 +7,19 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet var importantCheckBox: NSButton!
+    @IBOutlet var textField: NSTextField!
+    
+    var todoItems: [TodoItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        getTodoItems()
     }
 
     override var representedObject: Any? {
@@ -20,7 +27,39 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    func getTodoItems() {
+        if let context = (NSApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            do {
+                todoItems = try context.fetch(TodoItem.fetchRequest())
+                print(todoItems.count)
+            } catch {
+                
+            }
+        }
+    }
 
-
+    @IBAction func submitClicked(_ sender: Any) {
+        if textField.stringValue != "" {
+            if let context = (NSApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let todoItem = TodoItem(context: context)
+                todoItem.name = textField.stringValue
+                if importantCheckBox.state == .off {
+                    todoItem.important = false
+                } else {
+                    todoItem.important = true
+                }
+                (NSApplication.shared.delegate as? AppDelegate)?.saveAction(nil)
+                textField.stringValue = ""
+                importantCheckBox.state = .off
+                getTodoItems()
+            }
+        }
+    }
+    
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return todoItems.count
+    }
 }
 
