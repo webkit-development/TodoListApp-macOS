65621a73cc91a48abfe94a8f0c4824e70fddce57
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-28 13:38:10 -0400

    Initial Commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by Kevin Stradtman on 3/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
